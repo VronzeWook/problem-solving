58239b2bb9c788ddbc614192290d1babbc4373b8
VronzeWook <94160933+VronzeWook@users.noreply.github.com>
2023-05-31 23:53:26 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,42 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int MAX = 54;
-int n, root_idx, cut, cnt;
+const int MAX = 50;
+int n, del_num, temp, cnt, root;
 vector<int> adj[MAX];
-int tree[MAX], temp[MAX];
 
-void DFS(int a) {
-	if (adj[a].empty()) {
+void dfs(int r) {
+	if (adj[r].empty()) {
 		cnt++;
 		return;
 	}
-	for (int i = 0; i < adj[a].size(); i++) {
-		DFS(adj[a][i]);
+	for (int i = 0; i < adj[r].size(); i++) {
+		dfs(adj[r][i]);
 	}
+
 	return;
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-
+	//입력
 	cin >> n;
-	for (int i = 0; i < n; i++) cin >> temp[i];
-	cin >> cut;
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		if (temp == -1) {
+			root = i;
+			continue;
+		}
+		adj[temp].push_back(i);
+	}
+	
+	cin >> del_num;
 
 	for (int i = 0; i < n; i++) {
-		if (temp[i] == -1) root_idx = i;
-		else if (i == cut) continue;
-		else {
-			adj[temp[i]].push_back(i);
+		for (int j = 0; j < adj[i].size(); j++) {
+			if (adj[i][j] == del_num) adj[i].erase(adj[i].begin() + j);
 		}
 	}
+	
+	dfs(root);
 
-	DFS(root_idx);
-
-	if (root_idx == cut) cnt = 0;
-	cout << cnt;
+	if (del_num == root) cout << 0;
+	else cout << cnt;
 
 	return 0;
 }
