6502d36fe2d6837b33d80c7cc282733bf9dafbc1
VronzeWook <94160933+VronzeWook@users.noreply.github.com>
2023-06-01 15:46:34 +0900

    [Silver I] Title: 효율적인 해킹, Time: 3204 ms, Memory: 3560 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -2,15 +2,18 @@
 using namespace std;
 
 const int MAX = 10004;
-vector<int> adj[MAX];
-int n, m, cnt, a, b, mx;
-int ret_temp[MAX], visited[MAX];
+int n, m, temp1, temp2, cnt, mx, _prev_mx, cnt_ret[MAX];
+vector<int> adj[MAX], ret;
+bool visited[MAX];
 
-void DFS(int a) {
-	if (visited[a]) return;
-	visited[a] = 1;
+//노드 완전 탐색으로 노드 개수 카운트
+void dfs(int r) {
+	visited[r] = 1;
+	for (int i = 0; i < adj[r].size(); i++) {
+		if (visited[adj[r][i]]) continue;
+		dfs(adj[r][i]);
+	}
 	cnt++;
-	for (int i = 0; i < adj[a].size(); i++) DFS(adj[a][i]);
 	return;
 }
 
@@ -18,22 +21,31 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
+	//입력 및 인접 리스트 기록
 	cin >> n >> m;
-	
-	for (int i = 1; i <= m; i++) {
-		cin >> a >> b;
-		adj[b].push_back(a);
+	for (int i = 0; i < m; i++) {
+		cin >> temp1 >> temp2;
+		adj[temp2].push_back(temp1);
+	}
+
+	//모든 노드에서 탐색하며 전파가능한 최대 노드 수 갱신
+	for (int i = 1; i <= n; i++) {
+		fill(&visited[0], &visited[0] + MAX, 0);
+		cnt = 0;
+		dfs(i);
+		mx = max(cnt, mx);
+		cnt_ret[i] = cnt;
 	}
 
 	for (int i = 1; i <= n; i++) {
-		fill(&visited[0], &visited[MAX], 0);
-		cnt = 0;
-		DFS(i);
-		ret_temp[i] = cnt;
+		if (cnt_ret[i] == mx) ret.push_back(i);
 	}
 	
-	for (int i = 1; i <= n; i++) if (mx < ret_temp[i]) mx = ret_temp[i];
-	for (int i = 1; i <= n; i++) if (ret_temp[i] == mx) cout << i << " ";
+	sort(ret.begin(), ret.end());
+
+	for (int i = 0; i < ret.size(); i++) {
+		cout << ret[i] << " ";
+	}
 
 	return 0;
 }
