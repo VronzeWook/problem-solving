82c1dfdd1d89bf9d71a3436ce80f767d81e16e3a
VronzeWook <94160933+VronzeWook@users.noreply.github.com>
2023-06-01 13:25:12 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,42 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const int NMAX = 1002;
-int n, mx;
-ll c;
-pair<ll, bool> in[NMAX];
-vector<pair<ll, int>> cnt;
 
+int n, c, temp;
+vector<pair<int, int>> num; //숫자, 횟수
+
+bool cmp(const pair<int, int> &a, const pair<int, int> &b) {
+	return a.second > b.second;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
+	//입력
 	cin >> n >> c;
 	for (int i = 0; i < n; i++) {
-		cin >> in[i].first;
-	}
-	for (int i = 0; i < n ; i++) {
-		if (in[i].second == true) continue;
-		ll current = in[i].first;
-		cnt.push_back({current, 1 });
-		size_t last = cnt.size() - 1;
-		for (int j = i + 1; j < n; j++) {
-			if (in[j].first == current) {
-				cnt[last].second++;
-				in[j].second = true;
+		int flag = false;
+		cin >> temp;
+
+		//벡터에 인풋이 이미 있는지 검사
+		for (int j = 0; j < num.size(); j++) {
+			if (num[j].first == temp) {
+				flag = true;
+				num[j].second++;
 			}
 		}
-		if (cnt[last].second > mx) mx = cnt[last].second;
+
+		//새로 들어온 숫자일 경우
+		if (!flag) num.push_back({ temp, 1 });
+		
+	}
+    
+    //정렬 (바뀌지 않을 때 기존 순서 보장 stable_sort)
+	stable_sort(num.begin(), num.end(), cmp);
+
+    //출력
+	for (int i = 0; i < num.size(); i++) {
+		for (int j = 0; j < num[i].second; j++) {
+			cout << num[i].first << " ";
+		}
 	}
 
-	for (int i = mx; i > 0; i--) {
-		for (int j = 0; j < cnt.size(); j++) {
-			if (cnt[j].second == i) {
-				for(int k = 0; k < cnt[j].second; k++) cout << cnt[j].first << " ";
-			}
-		}
-	}
-	cout << endl;
 	return 0;
 }
