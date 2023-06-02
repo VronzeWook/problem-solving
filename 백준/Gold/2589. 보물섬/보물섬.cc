9b7b72540f9864f4f9141e8ea3829446ab37d8b5
VronzeWook <94160933+VronzeWook@users.noreply.github.com>
2023-06-02 16:49:57 +0900

    [Gold V] Title: 보물섬, Time: 128 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,36 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int NMX = 51;
+const int MAX = 53;
+int visited[MAX][MAX], mp[MAX][MAX];
+int my[4] = { 1, 0, -1, 0 }, mx[4] = { 0, 1, 0, -1 };
 int n, m, ret;
-int visited[NMX][NMX], my[4] = {1, 0, -1, 0}, mx[4] = {0, 1, 0, -1};
 char temp;
-bool mp[NMX][NMX];
 
-int bfs(int y, int x) {
-	if (!mp[y][x]) return 0;
-	//cout << "in bfs\n";
-	int mx_cnt = 0;
-	queue < pair<int, int>> q;
-	q.push({ y, x });
+void BFS(int y, int x) {
+	queue<pair<int, int>> q;
 	visited[y][x] = 1;
-
-	do {
+	q.push({ y, x });
+	while (q.size()) {
+		int now_y = q.front().first;
+		int now_x = q.front().second;
 		for (int i = 0; i < 4; i++) {
-			int ny = my[i] + q.front().first;
-			int nx = mx[i] + q.front().second;
-			if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] != 0 || !mp[ny][nx]) continue;
-			visited[ny][nx] = visited[q.front().first][q.front().second] + 1;
-			mx_cnt = max(mx_cnt, visited[ny][nx]);
-			//cout << "push :" << ny << "," << nx << "\n";
+			int ny = now_y + my[i];
+			int nx = now_x + mx[i];
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || !mp[ny][nx])continue;
+			visited[ny][nx] = visited[now_y][now_x] + 1;
+			ret = max(ret, visited[ny][nx] - 1);
 			q.push({ ny, nx });
 		}
-		//cout << "pop :" << q.front().first << "," << q.front().second << "\n";
 		q.pop();
-	} while (q.size());
-	//cout << "\n";
-
-	return mx_cnt;
+	}
+	return;
 }
 
 int main() {
@@ -48,14 +42,13 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			fill(&visited[0][0], &visited[0][0] + NMX * NMX, 0);
-			ret = max(bfs(i, j), ret);
-			//cout << "i  j : " << i << ", " << j << "\n";
+			if (!mp[i][j]) continue;
+			fill(&visited[0][0], &visited[0][0] + MAX * MAX, 0);
+			BFS(i, j);
 		}
 	}
-
-	if (ret == 0) ret += 1;
-	cout << ret - 1 << "\n";
+	
+	cout << ret;
 
 	return 0;
 }
