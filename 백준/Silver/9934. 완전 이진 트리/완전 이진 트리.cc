d7d7abe229ab5426e9cec64345bb2fda3c3eb300
VronzeWook <94160933+VronzeWook@users.noreply.github.com>
2023-06-02 14:13:59 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2348 KB -BaekjoonHub

---
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX = 2048;
+int arr[MAX], k, n;
+vector<int> bi_tree[13];
+
+void make_bi(int _begin, int _end, int _layer){
+	//리프 노드 도달 시 추가
+	if (_layer == k) {
+		bi_tree[_layer].push_back(_end);
+		return;
+	}
+
+	//피벗 구하기
+	int pivot = (_begin + _end) / 2;
+
+	bi_tree[_layer].push_back(arr[pivot]);
+	//왼쪽 탐색
+	make_bi(_begin, pivot - 1, _layer + 1);
+	//오른쪽 탐색
+	make_bi(pivot + 1, _end, _layer + 1);
+
+	return;
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	//입력
+	cin >> k;
+	n = pow(2, k) - 1;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	//이진 트리 만들기
+	make_bi(0, n - 1, 0);
+	
+	//출력
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < bi_tree[i].size(); j++) {
+			cout << bi_tree[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
