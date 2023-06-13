576d4dc2d0bb1c56326a470d169b4eb2597dc18b
VronzeWook <94160933+VronzeWook@users.noreply.github.com>
2023-06-13 11:29:33 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3176 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,52 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int INF = 987654321;
-int scv[3], n, visited[64][64][64], ret;
-int _a[6][3] = {
+int atk[6][3] = {
 	{9, 3, 1},
 	{9, 1, 3},
-	{3, 1, 9},
 	{3, 9, 1},
-	{1, 3, 9},
-	{1, 9, 3}
+	{3, 1, 9},
+	{1, 9, 3},
+	{1, 3, 9}
 };
 
-struct thr {
-	int a; int b; int c;
+struct scvs {
+	int a, b, c;
 };
 
-int solve(int a, int b, int c) {
-	queue<thr> q;
-	q.push({ a, b, c });
-	visited[a][b][c] = 1;
-	do {
-		int a = q.front().a;
-		int b = q.front().b;
-		int c = q.front().c;
-		q.pop();
-		if (visited[0][0][0]) break;
-		for (int i = 0; i < 6; i++) {
-			int na = max(0, a - _a[i][0]);
-			int nb = max(0, b - _a[i][1]);
-			int nc = max(0, c - _a[i][2]);
-			if (visited[na][nb][nc]) continue;
-			visited[na][nb][nc] = visited[a][b][c] + 1;
-			q.push({ na, nb, nc });
-		}
-	} while (q.size());
-	return visited[0][0][0] - 1;
-}
+int visited[64][64][64];
+
+int n, cnt, scv[3];
+queue<scvs> q;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	
+
+	//입력
 	cin >> n;
+	
 	for (int i = 0; i < n; i++) {
 		cin >> scv[i];
 	}
+
+	//BFS
+	q.push({ scv[0], scv[1], scv[2] });
+	visited[scv[0]][scv[1]][scv[2]] = 1;
+	while (q.size()){
+		int a = q.front().a;
+		int b = q.front().b;
+		int c = q.front().c;
+		q.pop();
 	
-	cout << solve(scv[0], scv[1], scv[2]) << "\n";
+		for (int i = 0; i < 6; i++) {
+			int na = a - atk[i][0];
+			int nb = b - atk[i][1];
+			int nc = c - atk[i][2];
+			if (na < 0) na = 0;
+			if (nb < 0) nb = 0;
+			if (nc < 0) nc = 0;
+			if (visited[na][nb][nc]) continue;
+			visited[na][nb][nc] = visited[a][b][c] + 1;
+			q.push({ na, nb, nc });
+		}
+		if (visited[0][0][0]) break;
+	}
+
+	//출력
+	cout << visited[0][0][0] - 1;
+
+
 	return 0;
 }
