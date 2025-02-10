318eeb60c0c6dfaf4f546f4173172671e43534e5
VronzeWook <chanel0222@naver.com>
2025-02-10 15:26:42 +0900

    1주 5번 문제풀이

--- 스터디 문제 모음/1-5-Baekjoon-1931.cpp
+++ 스터디 문제 모음/1-5-Baekjoon-1931.cpp
@@ -8,7 +8,7 @@ int n, result, t;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
+    
     // 가장 많은 회의를 진행할 수 있는 수
     // 가정) 가장 빨리 끝나는 회의를 선택하는 것이 최적해
     // 증명?) 가장 빨리 끝나는 회의 이후의 회의를 선택해서 최대값을 구하더라도, 가장 빨리 끝나는 회의와 시간차 x
@@ -16,7 +16,6 @@ int main() {
     
     //입력
     cin >> n;
-
     for(int i = 0; i < n; i++){
         cin >> table[i].second >> table[i].first;
     }
